4a713bea018b03ea48750466cc528f81c6587e27
lrbn86 <lrbn86@gmail.com>
2020-02-06 00:05:40 -0500

    Added ability to show note on noteInputBody when clicking on a note

--- app.js
+++ app.js
@@ -13,12 +13,24 @@ createNewNoteBtn.addEventListener("click", function() {
     var noteBlock = document.createElement("li");
     noteID++;
     noteBlock.id = "noteBlock" + noteID;
-    noteBlock.className = "noteBlock";
+    noteBlock.className = "noteBlock unselectable";
+    noteBlock.name = "noteBlock";
     noteBlock.textContent = noteBodyInput.value;
     notes.append(noteBlock);
-    console.log(noteBlock.textContent);
   }
     noteBodyInput.value = "";
     noteBodyInput.focus();
-
 });
+
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement;
+}
+
+notes.onclick = function(event) {
+  const target = event.target;
+  if (target.classList[0] === "noteBlock") {
+    noteBodyInput.value = target.textContent;
+    noteBodyInput.focus();
+  }
+};
