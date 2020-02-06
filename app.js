a316a26070589580a4e4740bc9ce7fdc444a5660
lrbn86 <lrbn86@gmail.com>
2020-02-05 23:35:05 -0500

    Did a complete revamp. Struggling on how to keep beginning whitespace for innerHTML in targetNote

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const noteBodyInput = document.getElementById("noteBodyInput");
 const notes = document.getElementById("notes");
+const createNewNoteBtn = document.getElementById("createNewNoteBtn");
 
 var noteID = 0;
-
 var selectedNote;
 
 noteBodyInput.focus();
 noteBodyInput.value = "";
 
-noteBodyInput.addEventListener("input", function() {
-  // Check if ul contains any li's
-  // if it does, then create a new li
-  // else ...What do we do if it does have some li's
-  if(!notes.hasChildNodes() && noteBodyInput.value.length >= 1) {
-    const noteBlock = document.createElement("li");
-    noteBlock.innerHTML = noteBodyInput.value;
+createNewNoteBtn.addEventListener("click", function() {
+  if (noteBodyInput.value.trim().length >= 1) {
+    var noteBlock = document.createElement("li");
+    noteID++;
     noteBlock.id = "noteBlock" + noteID;
     noteBlock.className = "noteBlock";
-    selectedNote = noteBlock.id;
+    noteBlock.textContent = noteBodyInput.value;
     notes.append(noteBlock);
-  } else {
-    // Check what li we are on
-    var targetNote = document.getElementById(selectedNote);
-    if (noteBodyInput.value.length < 1) {
-      notes.removeChild(targetNote);
-    }
-    targetNote.innerHTML = noteBodyInput.value;
-    console.log(targetNote.innerHTML);
-
+    console.log(noteBlock.textContent);
   }
+    noteBodyInput.value = "";
+    noteBodyInput.focus();
+
 });
