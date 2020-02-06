ab7f87f55a31f4da2527985f1e3b3bbdfdf2a91f
lrbn86 <lrbn86@gmail.com>
2020-02-06 18:06:50 -0500

    Struggling with ordering of li's (notes)

--- app.js
+++ app.js
@@ -6,22 +6,29 @@ const notes = document.getElementById("notes");
 const createNewNoteBtn = document.getElementById("createNewNoteBtn");
 const selectedNotes = document.getElementsByClassName("selected");
 
+var notesArray = [];
+
 var noteID = sessionStorage.getItem("noteID");
 var selectedNote;
 
 function initSetup() {
-  noteID = 0;
-  for (let i = 1; i < sessionStorage.length; i++) {
-    const item = sessionStorage.getItem("noteBlock" + i);
-    var noteBlock = createNote(item);
-    noteBlock.addEventListener("click", selectedFunc);
-    notes.prepend(noteBlock);
+  if (sessionStorage.getItem("notesArray") != null) {
+    noteID = 0;
+    notesArray = sessionStorage.getItem("notesArray").split(",");
+    console.log(notesArray);
+    for (let i = 0; i < notesArray.length; i++) {
+      var content = sessionStorage.getItem(notesArray[i]);
+      var noteBlock = createNote(content);
+      notes.prepend(noteBlock);
+    }
   }
 }
 initSetup();
 
 function createNote(content) {
   var noteBlock = document.createElement("li");
+  noteBlock.addEventListener("click", selectedFunc);
+  noteBlock.addEventListener("dblclick", deleteFunc);
   noteID++;
   sessionStorage.setItem("noteID", noteID);
   noteBlock.id = "noteBlock" + noteID;
@@ -34,14 +41,21 @@ function createNote(content) {
 createNewNoteBtn.addEventListener("click", function() {
   if (noteBodyInput.value.trim().length >= 1) {
     var noteBlock = createNote(noteBodyInput.value);
-    noteBlock.addEventListener("click", selectedFunc);
     sessionStorage.setItem(noteBlock.id, noteBlock.textContent);
+    notesArray.push(noteBlock.id);
+
+    sessionStorage.setItem("notesArray", notesArray);
     notes.prepend(noteBlock);
   }
     noteBodyInput.value = "";
     noteBodyInput.focus();
-    notes.scrollTop = notes.scrollHeight;
     resetSelections();
+});
+
+noteBodyInput.addEventListener("input", function() {
+  if (noteBodyInput.value.trim().length < 1) {
+    resetSelections();
+  }
 });
 
 function resetSelections() {
@@ -55,4 +69,17 @@ function selectedFunc(e) {
   e.target.classList.add("selected");
   noteBodyInput.value = e.target.textContent;
   noteBodyInput.focus();
+}
+
+// TODO: Problem: In our initSetup(), we always start at 1, so noteBlock1. If we delete that block, we will get an empty li
+function deleteFunc(e) {
+  sessionStorage.removeItem(e.target.id);
+  document.getElementById(e.target.id).remove();
+  var removedIndex = notesArray.indexOf(e.target.id);
+  notesArray.splice(removedIndex, 1);
+  console.log(notesArray);
+  sessionStorage.setItem("notesArray", notesArray);
+  noteID--;
+  sessionStorage.setItem("noteID", noteID);
+  noteBodyInput.value = "";
 }
