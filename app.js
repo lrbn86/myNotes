d192b4c6ba764dd948c06c5982fe4ce958415fc2
lrbn86 <lrbn86@gmail.com>
2020-02-06 19:14:26 -0500

    Redesigning logic for how to store notes on local/session storage

--- app.js
+++ app.js
@@ -8,20 +8,10 @@ const selectedNotes = document.getElementsByClassName("selected");
 
 var notesArray = [];
 
-var noteID = sessionStorage.getItem("noteID");
-var selectedNote;
-
 function initSetup() {
-  if (sessionStorage.getItem("notesArray") != null) {
-    noteID = 0;
-    notesArray = sessionStorage.getItem("notesArray").split(",");
-    console.log(notesArray);
-    for (let i = 0; i < notesArray.length; i++) {
-      var content = sessionStorage.getItem(notesArray[i]);
-      var noteBlock = createNote(content);
-      notes.prepend(noteBlock);
-    }
-  }
+  // When page is freshly loaded, we want to check if there exists notes already
+  // If there are notes in storage, then render them
+  // TODO:
 }
 initSetup();
 
@@ -29,35 +19,31 @@ function createNote(content) {
   var noteBlock = document.createElement("li");
   noteBlock.addEventListener("click", selectedFunc);
   noteBlock.addEventListener("dblclick", deleteFunc);
-  noteID++;
-  sessionStorage.setItem("noteID", noteID);
-  noteBlock.id = "noteBlock" + noteID;
   noteBlock.className = "noteBlock";
+  // TODO:
   noteBlock.textContent = content;
   return noteBlock;
 }
 
 // Listen for new notes being added
 createNewNoteBtn.addEventListener("click", function() {
+  // Make sure there are no blanks
   if (noteBodyInput.value.trim().length >= 1) {
-    var noteBlock = createNote(noteBodyInput.value);
-    sessionStorage.setItem(noteBlock.id, noteBlock.textContent);
-    notesArray.push(noteBlock.id);
-
-    sessionStorage.setItem("notesArray", notesArray);
-    notes.prepend(noteBlock);
+    // TODO:
   }
     noteBodyInput.value = "";
     noteBodyInput.focus();
     resetSelections();
 });
 
+// If user selects a note, but then erases the note's content, then it will deselect.
 noteBodyInput.addEventListener("input", function() {
   if (noteBodyInput.value.trim().length < 1) {
     resetSelections();
   }
 });
 
+// Removes selection indicator on all notes
 function resetSelections() {
   for (let i = 0; i < selectedNotes.length; i++) {
     selectedNotes[i].classList.remove("selected");
@@ -71,15 +57,9 @@ function selectedFunc(e) {
   noteBodyInput.focus();
 }
 
-// TODO: Problem: In our initSetup(), we always start at 1, so noteBlock1. If we delete that block, we will get an empty li
+// TODO:
 function deleteFunc(e) {
-  sessionStorage.removeItem(e.target.id);
-  document.getElementById(e.target.id).remove();
-  var removedIndex = notesArray.indexOf(e.target.id);
-  notesArray.splice(removedIndex, 1);
-  console.log(notesArray);
-  sessionStorage.setItem("notesArray", notesArray);
-  noteID--;
-  sessionStorage.setItem("noteID", noteID);
+
   noteBodyInput.value = "";
 }
+
